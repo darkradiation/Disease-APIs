94e567e428689b47e8aca88ccc5493c2e2647e1a
vishal kumar shaw <vishalkumar.msit@gmail.com>
2024-10-03 19:28:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -40,5 +40,5 @@ def predict_diabetes():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
